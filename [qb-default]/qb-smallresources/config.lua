1d5988e78e9da0f446a51133f0566c9e51d886b7
FLY;( <162630159+XStoryRp@users.noreply.github.com>
2024-03-07 22:35:18 +0200

    Manual Gear

--- [qb-default]/qb-smallresources/config.lua
+++ [qb-default]/qb-smallresources/config.lua
@@ -714,6 +714,26 @@ Config.Bikes = {
     },
 }
 
+-- GearBox
+
+
+Config.Debug = false -- toggle this on if you have issues, BEFORE asking for help
+Config.OxLib = false -- set to true if you use Oxlib, will otherwise default to QBcore. Make sure to double check fxmanifest if this is included or not
+
+Config.CwTuning = false -- Set to true if you use CW tuning and want to use that scripts gearbox swaps
+Config.UseOtherCheck = false -- set to true and add your code to client.lua. Search for "ADD YOUR CHECK HERE" in the file
+
+Config.ClutchTime = 400 -- value / clutchChangeRate As standard, GTA uses 900 for this value but I found that lower values works better. Higher means slower gearing.
+
+Config.Keys = {
+    gearUp = 'UP',
+    gearDown = 'DOWN'
+}
+
+Config.rhdCars = {
+    -- "carNameHere",
+}
+
 
 Config.UseLanguage = "en"
 Config.UseSoundEffect = true
